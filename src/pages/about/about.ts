5eec4e8cbf64dd5bb1f7ac67d8e659c1a306c6d5
Yera Díaz <aray.dr@gmail.com>
2017-07-16 16:53:49 +0100

    some style changes

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -18,6 +18,7 @@ export class AboutPage {
   @ViewChild('lineCanvas') lineCanvas;
   @ViewChild('barCanvas') barCanvas;
   lineChart: any;
+  lineChart2: any;
   barChart: any;
   objectString : any;
 
@@ -104,6 +105,37 @@ export class AboutPage {
                 datasets: [
                     {
                         label: "My First dataset",
+                        fill: false,
+                        lineTension: 0.1,
+                        backgroundColor: "rgba(75,192,192,0.4)",
+                        borderColor: "rgba(75,192,192,1)",
+                        borderCapStyle: 'butt',
+                        borderDash: [],
+                        borderDashOffset: 0.0,
+                        borderJoinStyle: 'miter',
+                        pointBorderColor: "rgba(75,192,192,1)",
+                        pointBackgroundColor: "#fff",
+                        pointBorderWidth: 1,
+                        pointHoverRadius: 5,
+                        pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                        pointHoverBorderColor: "rgba(220,220,220,1)",
+                        pointHoverBorderWidth: 2,
+                        pointRadius: 1,
+                        pointHitRadius: 10,
+                        data: [65, 59, 80, 81, 56, 55, 40],
+                        spanGaps: false,
+                    }
+                ]
+            }
+
+        });
+        this.lineChart2 = new Chart(this.lineCanvas.nativeElement, {
+            type: 'line',
+            data: {
+                labels: ["January", "February", "March", "April", "May", "June", "July"],
+                datasets: [
+                    {
+                        label: "My second dataset",
                         fill: false,
                         lineTension: 0.1,
                         backgroundColor: "rgba(75,192,192,0.4)",
